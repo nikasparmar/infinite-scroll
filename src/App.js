99848e5af8899aced01d3e7bc873d1da80e7f81d
nikas-dmartindia <nikas.parmar@dmartindia.com>
2022-10-31 16:14:29 +0530

    bug:fixes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
   useEffect(() => {
     const fetchItems = async () => {
       const response = await axios.get(`https://englishapi.xynie.com/app-api/v1/photo-gallery-feed-page/page/${page}`);
-      setData(response?.data?.nodes)
+      const responseCopy = [...data,...response?.data?.nodes];
+      setData(responseCopy)
     }
     fetchItems()
   }, [page]);
@@ -38,28 +39,30 @@ function App() {
           data?.map((ele) => (
             <div key={ele?.node?.nid} className="rounded-md flex flex-col lg:flex-row w-full my-5 shadow-default"  >
               <div className='h-40 w-full lg:w-40'>
-                <img src={ele?.node?.ImageStyle_thumbnail} className="object-fit object-cover h-full w-full rounded-tr-md lg:rounded-tr-none rounded-tl-md lg:rounded-bl-md" />
+                <img src={ele?.node?.ImageStyle_thumbnail}
+                  loading="lazy"
+                  className="object-fit object-cover h-full w-full rounded-tr-md lg:rounded-tr-none rounded-tl-md lg:rounded-bl-md" />
               </div>
               <div className='p-3 lg:p-5 flex flex-col justify-between w-full'>
-                <p className='text-xl'>{ele?.node?.title}</p>
-                <p className='italic text-base'>Author: {ele?.node?.author_name}</p>
+                <div className='text-xl'>{ele?.node?.title}</div>
+                <div className='italic text-base'>Author: {ele?.node?.author_name}</div>
                 <div className='flex justify-between w-full'>
-                  <p className='text-sm md:text-base flex flex-col md:flex-row items-start'>
-                    <p>
+                  <div className='text-sm md:text-base flex flex-col md:flex-row items-start'>
+                    <div>
                       Total views
-                    </p>
-                    <p className='ml-0 md:ml-1'>
+                    </div>
+                    <div className='ml-0 md:ml-1'>
                       {ele?.node?.views_count}
-                    </p>
-                  </p>
-                  <p className='text-sm md:text-base flex items-center flex-col md:flex-row'>
-                    <p className=''>
+                    </div>
+                  </div>
+                  <div className='text-sm md:text-base flex items-center flex-col md:flex-row'>
+                    <div className=''>
                       Last updated
-                    </p>
-                    <p className='ml-0 md:ml-1'>
+                    </div>
+                    <div className='ml-0 md:ml-1'>
                       {moment(ele?.node?.last_update).format('DD-MM-YYYY')}
-                    </p>
-                  </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
